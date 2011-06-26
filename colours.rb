5b8b200e675ad9e99b3b63a56cdf782d4efb9a80
Sami Samhuri <sami.samhuri@gmail.com>
2011-06-26 15:50:49 -0700

    fix parts < 0xf in colours.rb

--- colours.rb
+++ colours.rb
@@ -4,6 +4,11 @@ hex = ''
 rgb = []
 red = green = blue = 0
 
+def to_hex n
+  s = n.to_i.to_s(16)
+  s.length == 1 ? '0' + s : s
+end
+
 if ARGV.size == 1
   hex = ARGV.first[0,1] == '#' ? ARGV.first[1..-1] : ARGV.first
   red = hex[0,2].to_i(16)
@@ -18,7 +23,7 @@ elsif ARGV.size == 3
     green = (255 * green.to_f).to_i
     blue = (255 * blue.to_f).to_i
   end
-  hex = [red, green, blue].map {|n| n.to_i.to_s(16)}.join
+  hex = [red, green, blue].map {|n| to_hex(n) }.join
 end
 
 puts '#' + hex
