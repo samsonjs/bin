0535c77a73494bb71ded006ad7aadf31c8e78fad
Sami Samhuri <sami.samhuri@gmail.com>
2011-11-26 23:02:09 -0800

    add publishing scripts

---
+++ publish-blog.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [[ -d ~/Projects/web/samhuri.net ]]; then
+	cd ~/Projects/web/samhuri.net
+elif [[ -d ~/samhuri.net ]]; then
+	cd ~/samhuri.net
+else
+	echo "error: samhuri.net directory not found"
+	exit 1
+fi
+
+cd _blog
+git clean -fdq
+git pull
+cd ..
+
+make publish_blog
---
+++ publish-samhuri.net.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [[ -d ~/Projects/web/samhuri.net ]]; then
+	cd ~/Projects/web/samhuri.net
+elif [[ -d ~/samhuri.net ]]; then
+	cd ~/samhuri.net
+else
+	echo "error: samhuri.net directory not found"
+	exit 1
+fi
+
+cd _blog
+git clean -fdq
+git pull
+cd ..
+
+git clean -fdq
+git pull
+
+make publish
