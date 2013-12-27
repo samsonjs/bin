825f9ab0d8d1a350f58c4e6efaf1cb8d51a655b8
Sami Samhuri <sami@samhuri.net>
2013-12-27 11:01:30 -0800

    fix restart-discussd.sh script

--- restart-discussd.sh
+++ restart-discussd.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-ps ax | grep 'node discussd' | grep -v grep | cut -f1 -d' ' | xargs kill
+ps ax | grep 'node discussd' | grep -v grep | awk '{print $1}' | xargs kill
 cd ~/discussd
 node discussd.js -h 0.0.0.0 -p 8000 >|discussd.log 2>>|discussd.log &!
